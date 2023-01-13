d51ea88e79af1543f53fb7fc3d6d2f3f2dd2c8e6
Radek Warakomski <sudawiec@icloud.com>
2023-01-13 21:06:11 +0100

    Add features bloop background image and .no-scroll class toggling

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const navToggle = document.querySelector(".nav-toggle");
 
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
+  document.body.classList.toggle("no-scroll");
 });
