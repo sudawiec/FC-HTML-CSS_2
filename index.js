4ba4e2c95350cc86698715d1e6e943308c6ac89c
Radek Warakomski <sudawiec@icloud.com>
2023-01-21 20:48:49 +0100

    Update testimonials section

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// Create variables to store DOM nodes
 const navToggle = document.querySelector(".nav-toggle");
-
+// Event listeners
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
   document.body.classList.toggle("no-scroll");
