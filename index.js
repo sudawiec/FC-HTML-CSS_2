de934814674dd2f8ecf50201aad24fd2815dfd07
Radek Warakomski <sudawiec@icloud.com>
2023-01-28 23:40:17 +0100

    Add some dropdown menu elements and styling

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+
 // Create variables to store DOM nodes
+
+const activeBtn = document.querySelector(".active");
+const dropdownMenu = document.querySelector("#dropdown")
 const navToggle = document.querySelector(".nav-toggle");
+
+// Function
+
+const toggleDropdown = () => {
+  dropdownMenu.classList.toggle("show");
+};
+
 // Event listeners
+
+activeBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+document.documentElement.addEventListener("click", () => {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
+  }
+});
+
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
   document.body.classList.toggle("no-scroll");
