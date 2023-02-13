5e893d4a7bd99661dae15a11463d01d09c050195
Radek Warakomski <sudawiec@icloud.com>
2023-02-13 20:50:55 +0100

    Change names and add menuList toggling

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 // Create variables to store DOM nodes
 
-const dropdownMenu = document.querySelector("#dropdown");
-const horizontalNavBtn = document.querySelector("#horizontal-nav__item");
-const navToggle = document.querySelector(".nav-toggle");
+// const dropdownMenu = document.querySelector("#dropdown");
+// const horizontalNavBtn = document.querySelector("#horizontal-nav__item");
+const menuToggleBtn = document.querySelector(".menu__toggle-btn");
+const menuList = document.querySelector(".menu__item-list");
 
 // Functions
 
-const toggleDropdown = () => {
-  dropdownMenu.classList.toggle("show");
-  horizontalNavBtn.classList.toggle("active");
-};
+// const toggleDropdown = () => {
+//   dropdownMenu.classList.toggle("show");
+//   horizontalNavBtn.classList.toggle("active");
+// };
 
 // Event listeners
 
-horizontalNavBtn.addEventListener("click", (e) => {
-  e.stopPropagation();
-  toggleDropdown();
-});
+// horizontalNavBtn.addEventListener("click", (e) => {
+//   e.stopPropagation();
+//   toggleDropdown();
+// });
 
-document.documentElement.addEventListener("click", () => {
-  if (dropdownMenu.classList.contains("show")) {
-    toggleDropdown();
-  }
-});
+// document.documentElement.addEventListener("click", () => {
+//   if (dropdownMenu.classList.contains("show")) {
+//     toggleDropdown();
+//   }
+// });
 
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
+menuToggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("menu__toggle-btn--is-open");
+  menuList.classList.toggle("menu__item-list--vertical");
   document.body.classList.toggle("no-scroll");
 });
